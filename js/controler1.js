b59f7ae12c23c84dbae21d41f4375925ebde60eb
zyq97 <272821699@qq.com>
2017-10-21 11:48:32 +0800

    test

---
+++ js/controler1.js
@@ -0,0 +1,54 @@
+
+function a(){
+	var one=document.getElementById("content11");
+	var two=document.getElementById("content22");
+	var three=document.getElementById("content33");
+	var four=document.getElementById("content44");
+	one.style.display="block";
+	two.style.display="none";
+	three.style.display="none";
+	four.style.display="none";
+}
+function b(){
+	var one=document.getElementById("content11");
+	var two=document.getElementById("content22");
+	var three=document.getElementById("content33");
+	var four=document.getElementById("content44");
+	one.style.display="none";
+	two.style.display="block";
+	three.style.display="none";
+	four.style.display="none";
+}
+function c(){
+	var one=document.getElementById("content11");
+	var two=document.getElementById("content22");
+	var three=document.getElementById("content33");
+	var four=document.getElementById("content44");
+	one.style.display="none";
+	two.style.display="none";
+	three.style.display="block";
+	four.style.display="none";
+}
+function d(){
+	var one=document.getElementById("content11");
+	var two=document.getElementById("content22");
+	var three=document.getElementById("content33");
+	var four=document.getElementById("content44");
+	one.style.display="none";
+	two.style.display="none";
+	three.style.display="none";
+	four.style.display="block";
+}
+function e(){
+	var one=document.getElementById("content221");
+	var two=document.getElementById("content222");
+	one.style.display="block";
+	two.style.display="none";
+}
+function f(){
+	var one=document.getElementById("content221");
+	var two=document.getElementById("content222");
+	one.style.display="none";
+	two.style.display="block";
+}
+
---
+++ js/sanjiao.js
@@ -0,0 +1,74 @@
+
+function aaa(){
+	document.getElementById("sanjiao0").style.opacity=1;
+	document.getElementById('sanjiao1').style.opacity=0;
+	document.getElementById('sanjiao2').style.opacity=0;
+	document.getElementById('sanjiao3').style.opacity=0;
+	document.getElementById('sanjiao4').style.opacity=0;
+	document.getElementById('sanjiao5').style.opacity=0;
+	document.getElementById("dropdownm").style.display="none"
+	document.getElementById("online").style.display="none"
+	document.getElementById("card").style.display="none";
+	document.getElementById("card1").style.display="none";
+	document.getElementById("card2").style.display="none";
+	document.getElementById("dropdownm1").style.display="none";
+}
+function bbb(){
+	document.getElementById("sanjiao0").style.opacity=0;
+	document.getElementById('sanjiao1').style.opacity=1;
+	document.getElementById('sanjiao2').style.opacity=0;
+	document.getElementById('sanjiao3').style.opacity=0;
+	document.getElementById('sanjiao4').style.opacity=0;
+	document.getElementById('sanjiao5').style.opacity=0;
+	document.getElementById("dropdownm").style.display="none"
+	document.getElementById("online").style.display="none"
+	document.getElementById("card").style.display="none";
+	document.getElementById("card1").style.display="none";
+	document.getElementById("card2").style.display="none";
+	document.getElementById("dropdownm1").style.display="none";
+}
+function ccc(){
+	document.getElementById("sanjiao0").style.opacity=0;
+	document.getElementById('sanjiao1').style.opacity=0;
+	document.getElementById('sanjiao2').style.opacity=1;
+	document.getElementById('sanjiao3').style.opacity=0;
+	document.getElementById('sanjiao4').style.opacity=0;
+	document.getElementById('sanjiao5').style.opacity=0;
+	document.getElementById("online").style.display="none";
+	document.getElementById("dropdownm").style.display="none";
+	document.getElementById("dropdownm1").style.display="block";
+}
+function ddd(){
+	document.getElementById("sanjiao0").style.opacity=0;
+	document.getElementById('sanjiao1').style.opacity=0;
+	document.getElementById('sanjiao2').style.opacity=0;
+	document.getElementById('sanjiao3').style.opacity=1;
+	document.getElementById('sanjiao4').style.opacity=0;
+	document.getElementById('sanjiao5').style.opacity=0;
+	document.getElementById("online").style.display="none"
+	document.getElementById("dropdownm").style.display="none"
+	document.getElementById("dropdownm1").style.display="none";
+}
+function eee(){
+	document.getElementById("sanjiao0").style.opacity=0;
+	document.getElementById('sanjiao1').style.opacity=0;
+	document.getElementById('sanjiao2').style.opacity=0;
+	document.getElementById('sanjiao3').style.opacity=0;
+	document.getElementById('sanjiao4').style.opacity=1;
+	document.getElementById('sanjiao5').style.opacity=0;
+	document.getElementById("online").style.display="block";
+	document.getElementById("dropdownm").style.display="none";
+	document.getElementById("card").style.display="none";
+	document.getElementById("card1").style.display="none";
+	document.getElementById("card2").style.display="none";
+	document.getElementById("dropdownm1").style.display="none";
+}
+function fff(){
+	document.getElementById("sanjiao0").style.opacity=0;
+	document.getElementById('sanjiao1').style.opacity=0;
+	document.getElementById('sanjiao2').style.opacity=0;
+	document.getElementById('sanjiao3').style.opacity=0;
+	document.getElementById('sanjiao4').style.opacity=0;
+	document.getElementById('sanjiao5').style.opacity=1;
+	document.getElementById("dropdownm").style.display="block"
+	document.getElementById("dropdownm1").style.display="none";}
